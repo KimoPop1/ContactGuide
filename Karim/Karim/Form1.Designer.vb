16c4d7a6d14f35ee751cc784acecfc90ca7f10ce
KimoPop1 <88283376+KimoPop1@users.noreply.github.com>
2021-08-01 18:20:10 +0200

    Add files via upload

---
+++ Karim/Karim/Form1.Designer.vb
@@ -0,0 +1,431 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.TextWorkAddress = New System.Windows.Forms.TextBox()
+        Me.TextHomeAddress = New System.Windows.Forms.TextBox()
+        Me.TextPhone1 = New System.Windows.Forms.TextBox()
+        Me.TextPhone2 = New System.Windows.Forms.TextBox()
+        Me.TextJobTitle = New System.Windows.Forms.TextBox()
+        Me.TextJobID = New System.Windows.Forms.TextBox()
+        Me.TextEmail = New System.Windows.Forms.TextBox()
+        Me.TextNotes = New System.Windows.Forms.TextBox()
+        Me.dgw = New System.Windows.Forms.DataGridView()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.LblID = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        CType(Me.dgw, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Work Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Home Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Phone1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 161)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Phone2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 199)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Job Title"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 237)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Job ID"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 275)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(31, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Email"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 313)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(35, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Notes"
+        '
+        'TextName
+        '
+        Me.TextName.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextName.Location = New System.Drawing.Point(92, 6)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(100, 20)
+        Me.TextName.TabIndex = 10
+        '
+        'TextWorkAddress
+        '
+        Me.TextWorkAddress.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextWorkAddress.Location = New System.Drawing.Point(92, 44)
+        Me.TextWorkAddress.Name = "TextWorkAddress"
+        Me.TextWorkAddress.Size = New System.Drawing.Size(100, 20)
+        Me.TextWorkAddress.TabIndex = 11
+        '
+        'TextHomeAddress
+        '
+        Me.TextHomeAddress.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextHomeAddress.Location = New System.Drawing.Point(92, 82)
+        Me.TextHomeAddress.Name = "TextHomeAddress"
+        Me.TextHomeAddress.Size = New System.Drawing.Size(100, 20)
+        Me.TextHomeAddress.TabIndex = 12
+        '
+        'TextPhone1
+        '
+        Me.TextPhone1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextPhone1.Location = New System.Drawing.Point(92, 120)
+        Me.TextPhone1.Name = "TextPhone1"
+        Me.TextPhone1.Size = New System.Drawing.Size(100, 20)
+        Me.TextPhone1.TabIndex = 13
+        '
+        'TextPhone2
+        '
+        Me.TextPhone2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextPhone2.Location = New System.Drawing.Point(92, 158)
+        Me.TextPhone2.Name = "TextPhone2"
+        Me.TextPhone2.Size = New System.Drawing.Size(100, 20)
+        Me.TextPhone2.TabIndex = 14
+        '
+        'TextJobTitle
+        '
+        Me.TextJobTitle.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextJobTitle.Location = New System.Drawing.Point(92, 196)
+        Me.TextJobTitle.Name = "TextJobTitle"
+        Me.TextJobTitle.Size = New System.Drawing.Size(100, 20)
+        Me.TextJobTitle.TabIndex = 15
+        '
+        'TextJobID
+        '
+        Me.TextJobID.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextJobID.Location = New System.Drawing.Point(92, 234)
+        Me.TextJobID.Name = "TextJobID"
+        Me.TextJobID.Size = New System.Drawing.Size(100, 20)
+        Me.TextJobID.TabIndex = 16
+        '
+        'TextEmail
+        '
+        Me.TextEmail.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextEmail.Location = New System.Drawing.Point(92, 272)
+        Me.TextEmail.Name = "TextEmail"
+        Me.TextEmail.Size = New System.Drawing.Size(100, 20)
+        Me.TextEmail.TabIndex = 17
+        '
+        'TextNotes
+        '
+        Me.TextNotes.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.TextNotes.Location = New System.Drawing.Point(92, 310)
+        Me.TextNotes.Name = "TextNotes"
+        Me.TextNotes.Size = New System.Drawing.Size(100, 20)
+        Me.TextNotes.TabIndex = 18
+        '
+        'dgw
+        '
+        Me.dgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgw.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column10, Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8, Me.Column9})
+        Me.dgw.Location = New System.Drawing.Point(215, 9)
+        Me.dgw.MultiSelect = False
+        Me.dgw.Name = "dgw"
+        Me.dgw.RowHeadersVisible = False
+        Me.dgw.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgw.Size = New System.Drawing.Size(671, 314)
+        Me.dgw.TabIndex = 19
+        '
+        'Column10
+        '
+        Me.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
+        Me.Column10.HeaderText = "ID"
+        Me.Column10.Name = "Column10"
+        Me.Column10.Width = 43
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Name"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 70
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Work Address"
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 70
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Home Address"
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 70
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Phone1"
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 70
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Phone2"
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 70
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Job Title"
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 70
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Job ID"
+        Me.Column7.Name = "Column7"
+        Me.Column7.Width = 70
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "Email"
+        Me.Column8.Name = "Column8"
+        Me.Column8.Width = 70
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = "Notes"
+        Me.Column9.Name = "Column9"
+        Me.Column9.Width = 70
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(717, 360)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 45)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Save Data"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(482, 360)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 45)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Clear Text"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(247, 360)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 45)
+        Me.Button3.TabIndex = 22
+        Me.Button3.Text = "Delete Data"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(12, 360)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(121, 45)
+        Me.Button4.TabIndex = 23
+        Me.Button4.Text = "Exit"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'LblID
+        '
+        Me.LblID.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.LblID.Location = New System.Drawing.Point(441, 329)
+        Me.LblID.Name = "LblID"
+        Me.LblID.ReadOnly = True
+        Me.LblID.Size = New System.Drawing.Size(33, 20)
+        Me.LblID.TabIndex = 24
+        Me.LblID.Visible = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(400, 329)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(31, 13)
+        Me.Label10.TabIndex = 25
+        Me.Label10.Text = "LblID"
+        Me.Label10.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(898, 417)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.LblID)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dgw)
+        Me.Controls.Add(Me.TextNotes)
+        Me.Controls.Add(Me.TextEmail)
+        Me.Controls.Add(Me.TextJobID)
+        Me.Controls.Add(Me.TextJobTitle)
+        Me.Controls.Add(Me.TextPhone2)
+        Me.Controls.Add(Me.TextPhone1)
+        Me.Controls.Add(Me.TextHomeAddress)
+        Me.Controls.Add(Me.TextWorkAddress)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Guide"
+        CType(Me.dgw, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextName As System.Windows.Forms.TextBox
+    Friend WithEvents TextWorkAddress As System.Windows.Forms.TextBox
+    Friend WithEvents TextHomeAddress As System.Windows.Forms.TextBox
+    Friend WithEvents TextPhone1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextPhone2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextJobTitle As System.Windows.Forms.TextBox
+    Friend WithEvents TextJobID As System.Windows.Forms.TextBox
+    Friend WithEvents TextEmail As System.Windows.Forms.TextBox
+    Friend WithEvents TextNotes As System.Windows.Forms.TextBox
+    Friend WithEvents dgw As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Column10 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents LblID As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+
+End Class
---
+++ Karim/Karim/Form1.vb
@@ -0,0 +1,206 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Reset()
+        If LblID.Text = "" Then
+            LblID.Text = "1"
+        Else
+            AutoNumber()
+        End If
+        GetData()
+    End Sub
+    Sub AutoNumber()
+        Try
+            con = New SqlConnection(cs)
+            con.Open()
+            cmd = New SqlCommand("Select  top (1) id from Table_1 ORDER BY id DESC", con)
+            rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            dgw.Rows.Clear()
+
+            While (rdr.Read() = True)
+
+                lblID.Text = rdr(0) + 1
+            End While
+
+            con.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetData()
+
+    End Sub
+    Sub GetData()
+        Try
+            con = New SqlConnection(cs)
+            con.Open()
+            cmd = New SqlCommand("SELECT  MasterID, Name, WorkAddress, HomeAddress, Phone1, Phone2, JobTitle, JobID, Email, Notes from TblMaster", con)
+            rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            dgw.Rows.Clear()
+
+            While (rdr.Read() = True)
+                dgw.Rows.Add(rdr(0), rdr(1), rdr(2), rdr(3), rdr(4), rdr(5), rdr(6), rdr(7), rdr(8), rdr(9))
+
+            End While
+            con.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+    Sub Reset()
+        TextName.Text = ""
+        TextWorkAddress.Text = ""
+        TextHomeAddress.Text = ""
+        TextPhone1.Text = ""
+        TextPhone2.Text = ""
+        TextJobTitle.Text = ""
+        TextJobID.Text = ""
+        TextEmail.Text = ""
+        TextNotes.Text = ""
+        LblID.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Len(Trim(TextName.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل الاسم", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextName.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextWorkAddress.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل عنوان العمل", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextWorkAddress.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextHomeAddress.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل عنوان المنزل", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextHomeAddress.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextPhone1.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل رقم الهاتف 1", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextPhone1.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextPhone2.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل رقم الهاتف 2", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextPhone2.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextJobTitle.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل المسمي الوظيفي", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextJobTitle.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextJobID.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل الرقم الوظيفي", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextJobID.Focus()
+            Exit Sub
+        End If
+
+        If Len(Trim(TextEmail.Text)) = 0 Then
+            MessageBox.Show("منفضلك ادخل الايميل", "حقل اجباري", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextEmail.Focus()
+            Exit Sub
+        End If
+        Try
+            con = New SqlConnection(cs)
+            con.Open()
+            Dim cb As String = "Insert into TblMaster(Name,WorkAddress,HomeAddress,Phone1,Phone2,JobTitle,JobID,Email,Notes)Values(@d1,@d2,@d3,@d4,@d5,@d6,@d7,@d8,@d9)"
+            cmd = New SqlCommand(cb)
+            cmd.Parameters.AddWithValue("@d1", TextName.Text)
+            cmd.Parameters.AddWithValue("@d2", TextWorkAddress.Text)
+            cmd.Parameters.AddWithValue("@d3", TextHomeAddress.Text)
+            cmd.Parameters.AddWithValue("@d4", TextPhone1.Text)
+            cmd.Parameters.AddWithValue("@d5", TextPhone2.Text)
+            cmd.Parameters.AddWithValue("@d6", TextJobTitle.Text)
+            cmd.Parameters.AddWithValue("@d7", TextJobID.Text)
+            cmd.Parameters.AddWithValue("@d8", TextEmail.Text)
+            cmd.Parameters.AddWithValue("@d9", TextNotes.Text)
+            cmd.Connection = con
+            Dim ms As New MemoryStream
+            cmd.ExecuteNonQuery()
+            con.Close()
+            MessageBox.Show("تم الحفظ بنجاح")
+            GetData()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub LblID_Click(sender As Object, e As EventArgs) Handles LblID.Click
+
+        '    LblID.Text = rdr(0) + 1
+
+    End Sub
+
+    Private Sub Values(p1 As Object, p2 As Object, p3 As Object, p4 As Object, p5 As Object, p6 As Object, p7 As Object, p8 As Object, p9 As Object, p10 As Object)
+        Throw New NotImplementedException
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DeleteData()
+    End Sub
+    Sub DeleteData()
+        Try
+            con = New SqlConnection(cs)
+            con.Open()
+            cmd = New SqlCommand("delete from TblMaster where MasterID=" & LblID.Text & "", con)
+            rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            con.Close()
+            MessageBox.Show("تم الحذف بنجاح")
+            GetData()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Reset()
+        GetData()
+
+    End Sub
+
+    Private Sub LblID_TextChanged(sender As Object, e As EventArgs) Handles LblID.TextChanged
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub dgw_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgw.CellClick
+        Dim vr As DataGridViewRow = dgw.SelectedRows(0)
+        LblID.Text = vr.Cells(0).Value.ToString
+        TextName.Text = vr.Cells(1).Value.ToString
+        TextWorkAddress.Text = vr.Cells(2).Value.ToString
+        TextHomeAddress.Text = vr.Cells(3).Value.ToString
+        TextPhone1.Text = vr.Cells(4).Value.ToString
+        TextPhone2.Text = vr.Cells(5).Value.ToString
+        TextJobTitle.Text = vr.Cells(6).Value.ToString
+        TextJobID.Text = vr.Cells(7).Value.ToString
+        TextEmail.Text = vr.Cells(8).Value.ToString
+        TextNotes.Text = vr.Cells(9).Value.ToString
+    End Sub
+
+    Private Sub dgw_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgw.CellContentClick
+
+    End Sub
+End Class
